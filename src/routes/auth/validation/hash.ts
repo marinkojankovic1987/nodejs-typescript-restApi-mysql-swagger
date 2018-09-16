d1919dd4800ac47655d4ea717c9679d5332dba4f
marinko <marinkojankovic87@gmail.com>
2018-09-16 16:58:12 +0200

    set-some-things-inswagger

--- src/routes/auth/validation/hash.ts
+++ src/routes/auth/validation/hash.ts
@@ -4,6 +4,7 @@ export default {
     postHash: {
         body: {
             email: Joi.string().email({ minDomainAtoms: 2 }).required(),
+            expired: Joi.boolean().required(),
         }
     },
     passwordHash: {
